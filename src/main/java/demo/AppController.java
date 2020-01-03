1028e1211c77f1e701cb2df0ba476a1d3a251150
Aleksandar Coso <aleksandar.coso@gmail.com>
2020-01-03 13:34:23 +1100

    Initial commit

---
+++ src/main/java/demo/AppController.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    Logger LOGGER = LoggerFactory.getLogger(AppController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        LOGGER.info("Loading application index...");
+        return "Hello World!";
+    }
+}
